222ce550fffdf6e617db39445f00367d24928a96
chubeloop <cnrywns1234@naver.com>
2024-11-11 22:47:49 +0900

    Update[feat]

--- app/src/main/java/com/example/volunteerkim/CommunityCrudActivity.java
+++ app/src/main/java/com/example/volunteerkim/CommunityCrudActivity.java
@@ -78,5 +78,25 @@ public class CommunityCrudActivity extends AppCompatActivity {
             }
         });
 
+        //Update
+        binding.btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DocumentReference postRef = db.collection("Boards").document("Review")
+                        .collection("Posts").document("Bu1pzXm18N7J9QvlpByi");
+
+                postRef.update("place", "updated place1", "content", "updated content1")
+                        .addOnSuccessListener(aVoid -> {
+                            Log.d("Firestore", "게시글 수정 성공");
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.w("Firestore", "게시글 수정 실패", e);
+                        });
+
+            }
+        });
+
+
     }
 }
