f2f290164c5d426f4a467734bf435c5ba267587d
Handher0 <handher0@naver.com>
2024-11-27 10:12:57 +0900

    yw+kj+sh3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
-<<<<<<< HEAD
-}
-=======
 }
 
 buildscript {
@@ -13,4 +10,3 @@ buildscript {
         mavenCentral()
     }
 }
->>>>>>> origin/feature/kj
