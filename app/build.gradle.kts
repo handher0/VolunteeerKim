fe4f127a2c325e969843504a004102d94b61a29d
chubeloop <cnrywns1234@naver.com>
2024-12-05 01:03:58 +0900

    add post recyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.8.9")
 
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
+
 
 
 }
