706141452dbb1e9a4e3f2e7aa39394b18b2c2fa5
Handher0 <handher0@naver.com>
2024-12-15 01:34:45 +0900

    dd

--- app/src/main/java/com/example/volunteerkim/CommunityFragment_other_post.java
+++ app/src/main/java/com/example/volunteerkim/CommunityFragment_other_post.java
@@ -14,7 +14,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;ㅈ
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
